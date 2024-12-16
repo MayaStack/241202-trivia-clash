e24dd0d47d1ff9e8f50628cd926ff2180d671df5
MayaStack <tam.stewing05@icloud.com>
2024-12-16 00:39:16 +0000

    added fullback fcts in the be

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,20 @@ app.get('/', (req, res) => {
     res.send({ message: 'Trivia Clash Backend is running!' });
 });
 
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    console.log('Headers:', req.headers);
+    next();
+});
+
+app.use((req, res) => {
+    res.status(404).send({ error: `Route not found: ${req.originalUrl}` });
+});
+
+app.get('/auth/postback/tunnel', (req, res) => {
+    res.status(200).send({ message: 'Tunnel postback received' });
+});
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
